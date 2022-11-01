1535f67f2e2cadedae95584ec538d6b6d63d625f
NguynHoangDuy <nguyenhoangduy933@gmail.com>
2022-11-01 20:32:17 +0700

    hoang duy

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nguyên liệu trà sữa</title>
+    <link rel="stylesheet" href="./assets/css/main.css"/>
+    
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    />
+</head>
+<body>
+    <?php
+        include("./block/connection.php");
+        include("./block/global.php");
+    ?>
+
+    <div class="container">
+        <h3>Thạch</h3>
+
+        <?php getSanPham($conn, "THA"); ?>
+    </div>
+        
+    <?php
+        include("./block/footer.php");
+    ?>
+</body>
+</html>
