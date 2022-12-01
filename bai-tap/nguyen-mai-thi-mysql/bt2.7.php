6751e756578ff134816e1a6008f255576282ca82
NguyenMaiThi0606 <maithy484@gmail.com>
2022-12-01 23:47:48 +0700

    tt

--- bai-tap/nguyen-mai-thi-mysql/bt2.7.php
+++ bai-tap/nguyen-mai-thi-mysql/bt2.7.php
@@ -17,8 +17,48 @@
         include("../../block/connection.php");
         // include("../block/global.php");
         include("../../block/header.php");
+        
     ?>
-<?php
+    <style>
+    a,
+    b {
+        text-decoration: none;
+        color: black;
+        font-size: 20px;
+        cursor: pointer;
+    }
+
+    .phantrang {
+        gap: 20px;
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .img-product {
+        box-sizing: border-box;
+        padding: 20px;
+    }
+
+    td {
+        text-align: center;
+        padding: 10px;
+    }
+    </style>
+    <style>
+    td,
+    th {
+        padding: 10px;
+        border: 1px solid grey;
+    }
+
+    table {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    </style>
+    <div class="container">
+    <?php
     $severname = "localhost";
     $username ="root";
     $password ="";
@@ -107,7 +147,9 @@
             echo "<a href=".$_SERVER ['PHP_SELF']."?page=".$lastPage."> >> </a>";  
             echo "</div>";
     ?>
-<?php
+    </div>
+
+    <?php
         include("../../block/footer.php");
     ?>
 </body>
